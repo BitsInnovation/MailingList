b7717343bf8827373a769ca9563e1674842b7b85
Christopher Bartos <bartosenator@gmail.com>
2014-08-09 22:14:29 -0400

    Celery for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn mailinglist.wsgi --log-file -
+worker: python manage.py celery worker -B -l info
--- mailinglist/celery.py
+++ mailinglist/celery.py
@@ -4,9 +4,9 @@ from celery import Celery
 from django.conf import settings
 
 # Indicate Celery to use the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailinglist.settings')
 
-app = Celery('myproject')
+app = Celery('mailinglist')
 app.config_from_object('django.conf:settings')
 # This line will tell Celery to autodiscover all your tasks.py that are in your app folders
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
