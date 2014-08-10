62eb82b1c2ab017985a7b99e549ee83d06646f0e
Christopher Bartos <bartosenator@gmail.com>
2014-08-09 23:13:49 -0400

    sending emails

--- list/tasks.py
+++ list/tasks.py
@@ -1,7 +1,7 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
-from datetime import datetime
+from datetime import date
 from list.models import EmailQueue
 from list.utils import send_email
 
@@ -10,7 +10,7 @@ logger = get_task_logger(__name__)
 @periodic_task(run_every=(crontab(hour="3", minute="5")), ignore_result=True)
 def send_emails():
   logger.info("Start send_emails() task...")
-  eq = EmailQueue.objects.filter(send_date=datetime.now(), sent=False)
+  eq = EmailQueue.objects.filter(send_date=date.today(), sent=False)
   count = 0
   for email in eq:
     success = send_email(email)
